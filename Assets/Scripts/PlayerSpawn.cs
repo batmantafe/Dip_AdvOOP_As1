45a0a0e26818157634efe1a8933fac9e71f77e4f
Duane Reymond <duane.reymond@gmail.com>
2018-04-04 16:08:25 +1000

    Working on Networking.

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+public class PlayerSpawn : NetworkBehaviour
+{
+    public GameObject mainCamera;
+
+    public GameObject gunnerGun, gunnerTurrent, playerHead, spaceship;
+    public bool isPilot, isGunner, nowCheckPlayer;
+
+    void Start()
+    {
+        Cursor.visible = false;
+
+        isPilot = false;
+        isGunner = false;
+
+        LocalPlayerSetup();
+    }
+
+    void Update()
+    {
+        PilotOrGunner();
+    }
+
+    void LocalPlayerSetup()
+    {
+        if (isLocalPlayer)
+        {
+            mainCamera.SetActive(true);
+        }
+    }
+
+    void PilotOrGunner()
+    {
+        if (nowCheckPlayer == true)
+        {
+            if (isPilot == true)
+            {
+                playerHead.GetComponent<PilotMouseLook>().enabled = true;
+            }
+
+            if (isGunner == true)
+            {
+                gunnerGun.SetActive(true);
+                gunnerTurrent.SetActive(true);
+
+                playerHead.GetComponent<PilotMouseLook>().enabled = false;
+
+                GetComponent<GunnerMouseLook>().enabled = true;
+
+                playerHead.GetComponent<GunnerMouseLook>().enabled = true;
+
+                spaceship.GetComponent<SpaceshipMovement>().enabled = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Pilot SPAWN")
+        {
+            transform.parent = other.gameObject.transform;
+            spaceship = other.gameObject.transform.root.gameObject;
+            isPilot = true;
+            nowCheckPlayer = true;
+
+
+        }
+
+        if (other.gameObject.name == "Gunner SPAWN")
+        {
+            transform.parent = other.gameObject.transform;
+            spaceship = other.gameObject.transform.root.gameObject;
+            isGunner = true;
+            nowCheckPlayer = true;
+        }
+    }
+}
---
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour
+{
+    public float forwardSpeed;
+    public float rotateSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        forwardSpeed = 50f;
+
+        rotateSpeed = 25f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForwardMovement();
+    }
+
+    void ForwardMovement()
+    {
+        transform.Translate(Vector3.forward * (forwardSpeed * Time.deltaTime));
+    }
+}
